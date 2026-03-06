351ce63fa1eff2461b30bb74f21c40657d55354d
Israfil <smisrafil158@gmail.com>
2026-03-06 11:22:16 -0600

    Some trash removed.

--- test-launcher.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-echo "Launcher would open here"
-zenity --info --text="Launcher Test" --width=300 --height=100
--- test-tools.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-echo "Tools would open here"
-zenity --list --title="Tools" --column="Tool" Terminal "File Manager" "Text Editor" --width=300 --height=200
